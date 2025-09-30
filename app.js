b4c0fcd92be2ce1de902cdbfd3cc3890643e2bf1
joni0202 <bikerjoniy@gmail.com>
2025-09-30 11:22:25 +0500

    feat: FrontEnd javascriptni qurish

--- app.js
+++ app.js
@@ -32,12 +32,7 @@ app.post("/create-item", (req, res) => {
   console.log("user entered / create-item ");
   const new_reja = req.body.reja;
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end("something went wrong");
-    } else {
-      res.end("seccessfully added");
-    }
+    res.json(data.ops[0]);
   });
 });
 
