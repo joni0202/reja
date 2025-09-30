6a5b0d5fe00c6b53fbffbd2013ef48725961d221
joni0202 <bikerjoniy@gmail.com>
2025-09-30 14:13:17 +0500

    fix: delete tugmasi ishlatildi

--- public/browser.js
+++ public/browser.js
@@ -38,3 +38,26 @@ document.getElementById("create-form").addEventListener("submit", function (e) {
       console.log("Iltimos qatadan harakat qiling!");
     });
 });
+
+document.addEventListener("click", function (e) {
+  // delete oper
+  console.log(e.target);
+  if (e.target.classList.contains("delete-me")) {
+    if (confirm("Aniq o'chirmoqchimisiz?")) {
+      axios
+        .post("/delete-item", { id: e.target.getAttribute("data-id") })
+        .then((respose) => {
+          console.log(respose.data);
+          e.target.parentElement.parentElement.remove();
+        })
+        .catch((err) => {
+          console.log("iltimos boshqatdan harakat qiling");
+        });
+    }
+  }
+
+  // edit oper
+  if (e.target.classList.contains("edit-me")) {
+    alert("siz editni bosdiz");
+  }
+});
